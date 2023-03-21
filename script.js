9bcae240dbd747ea4a9822f0c121a5015689f3fe
ArturKod <127652742+ArturKod@users.noreply.github.com>
2023-03-21 18:45:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+// 1. пирамида
+
+// let text = '';
+// let a = 8;
+// let count = 1;
+// for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < a; j++) {
+//         text += ' ';
+//     }
+//     for (let n = 0; n < count; n++) {
+//         text += '*';
+//     }
+//     count += 2;
+//     a--;
+//     text += '\n'
+// }
+// console.log(text)
+
+// 2. прямоугольный треугольник
+
+// let treugol = '';
+// let z = 1;
+// for (let i = 0; i < 7; i++) {
+//     for (let j = 0; j < z; j++) {
+//         treugol += '*';
+//     }
+//     z += 1;
+//     treugol += '\n'
+// }
+// console.log(treugol);
+
+// 3. 
+
+// let text = '';
+// let s = 7;
+// for (let i = 0; i < 7; i++) {
+//     for (let j = 0; j < s; j++) {
+//         text += '*'
+//     }
+//     text += '\n'
+//     s -= 1;
+// }
+// console.log(text);
+
+// 4.
+
+// let text ='';
+// let space = 0;
+// for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < space; j++) {
+//         text += ' ';
+//     }
+//     text += '*';
+//     space ++;
+//     text += '\n'
+// }
+//    console.log(text);
+
+// 5.
+
+// let text = '';
+// let space = 7;
+// for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < space; j++) {
+//         text += ' ';
+//     }
+//     text +='*';
+//     space --;
+//     text +='\n';
+// }
+// console.log(text);
+
+// 6.
+
+// let text = '';
+// for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < 5; j++) {
+//         text += '* ';
+//     }
+//     text += '\n'
+// }
+// console.log(text);
+
+// 7.
+
+// let text = '';
+// for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < 5; j++) {
+//         if (i == 0 || i == 7 || j == 0 || j == 4) {
+//             text += '* ';
+//         } else {
+//             text += '  ';
+//         }
+//     }
+//     text += '\n'
+// }
+// console.log(text);
+
+// 8.
+
+// let text = '';
+// let a = 8;
+// let count = 1;
+// let z = 2;
+// for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < a; j++) {
+//         text += ' ';
+//     }
+//     for (let n = 0; n < count; n++) {
+//         text += '*';
+//     }
+//     count += 2;
+//     a--;
+//     text += '\n'
+// }
+//     count -= 4;
+// for (let i = 0; i < 8; i++) {
+//     for (let j = 0; j < z; j++) {
+//         text += ' ';
+//     }
+//     z++;
+//     for (let n = 0; n < count; n++) {
+//         text += '*';
+//     }
+//     text += '\n';
+//     count -= 2;
+// }
+// console.log(text)
+
